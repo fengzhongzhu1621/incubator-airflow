2371487585227a67ece49085c2ec8560631f707c
fengzhongzhu1621 <jinyinqiao@gmail.com>
2019-01-05 18:33:18 +0800

    [IMP]: use xTool in cli.py

--- airflow/bin/cli.py
+++ airflow/bin/cli.py
@@ -76,6 +76,8 @@ from airflow.www_rbac.app import cached_appbuilder
 from sqlalchemy import func
 from sqlalchemy.orm import exc
 
+from xTool.utils.cli import BaseCLIFactory, Arg
+
 
 # 加载认证模块， 设置 api.api_auth.client_auth 全局变量
 api.load_auth()
@@ -1450,12 +1452,7 @@ def create_user(args):
         raise SystemExit('Failed to create user.')
 
 
-Arg = namedtuple(
-    'Arg', ['flags', 'help', 'action', 'default', 'nargs', 'type', 'choices', 'metavar'])
-Arg.__new__.__defaults__ = (None, None, None, None, None, None, None)
-
-
-class CLIFactory(object):
+class CLIFactory(BaseCLIFactory):
     args = {
         # Shared
         'dag_id': Arg(("dag_id",), "The id of the dag"),
@@ -2024,41 +2021,6 @@ class CLIFactory(object):
     subparsers_dict = {sp['func'].__name__: sp for sp in subparsers}
     dag_subparsers = (
         'list_tasks', 'backfill', 'test', 'run', 'pause', 'unpause')
-
-    @classmethod
-    def get_parser(cls, dag_parser=False):
-        # 创建命令行解析器
-        parser = argparse.ArgumentParser()
-        # 添加子解析器
-        subparsers = parser.add_subparsers(
-            help='sub-command help', dest='subcommand')
-        subparsers.required = True
-
-        # 获得需要添加到子解析器的命令名
-        subparser_name_list = cls.dag_subparsers if dag_parser else cls.subparsers_dict.keys()
-        # 遍历子解析器命令的名称
-        for subparser_name in subparser_name_list:
-            # 获得子解析器配置
-            subparser_conf = cls.subparsers_dict[subparser_name]
-            # 根据子命令名创建子解析器
-            sp = subparsers.add_parser(subparser_conf['func'].__name__,
-                                       help=subparser_conf['help'])
-            # 遍历子命令参数
-            for arg in subparser_conf['args']:
-                if 'dag_id' in arg and dag_parser:
-                    continue
-                # 根据参数名，从全局参数表中获取参数的详细配置
-                arg_namedtuple = cls.args[arg]
-                # 获得参数命名元组的值
-                kwargs = {
-                    f: getattr(arg_namedtuple, f)
-                    for f in arg_namedtuple._fields if f != 'flags' and getattr(arg_namedtuple, f)}
-                # 子解析器添加参数
-                # flags：参数的短写和长写
-                sp.add_argument(*arg_namedtuple.flags, **kwargs)
-            # 设置参数的动作
-            sp.set_defaults(func=subparser_conf['func'])
-        return parser
 
 
 def get_parser():
